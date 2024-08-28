40a20fec5f699922adb1b83a430a613d6cb550d4
김영훈 <148532120+hackers-mobile-dev-younghun@users.noreply.github.com>
2024-08-28 18:11:56 +0900

    - Renew project

---
+++ MyAudioPlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyAudioPlayer
+//
+//  Created by 김영훈 on 8/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyAudioPlayer/MyAudioPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAudioPlayerApp.swift
+//  MyAudioPlayer
+//
+//  Created by 김영훈 on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct MyAudioPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
